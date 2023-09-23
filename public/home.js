b79334446da762046fa80857ac5726c63056ab60
Daniyal-Qureshi <daniyalqureshi268@gmail.com>
2023-09-23 06:58:56 -0700

    added vercel json

--- public/home.js
+++ public/home.js
@@ -1,30 +1,36 @@
-import { ShiftKeysMapping } from "./keys.js";
-import { KeysMapping } from "./keys.js";
+// import { ShiftKeysMapping } from "./keys.js";
+// import { KeysMapping } from "./keys.js";
 
-$(document).ready(function () {
-  $("#target").keypress(function (event) {
-    if (event.key == "Enter") {
-      return;
-    }
-    var start = event.target.selectionStart;
-    var end = event.target.selectionEnd;
-    var oldValue = event.target.value;
-    var value;
 
-    if (event.shiftKey) {
-      value = ShiftKeysMapping[event.key.toUpperCase()]
-        ? ShiftKeysMapping[event.key.toUpperCase()]
-        : event.key;
-    } else {
-      value = KeysMapping[event.key.toUpperCase()]
-        ? KeysMapping[event.key.toUpperCase()]
-        : event.key;
-    }
 
-    var newValue = oldValue.slice(0, start) + value + oldValue.slice(end);
+// $(document).ready(function () {
+//   alert("Has been loaded")
+//   $("#target").keypress(function (event) {
+//     if (event.key == "Enter") {
+//       return;
+//     }
+//     var start = event.target.selectionStart;
+//     var end = event.target.selectionEnd;
+//     var oldValue = event.target.value;
+//     var value;
 
-    event.target.value = newValue;
-    event.target.selectionStart = event.target.selectionEnd = start + 1;
-    event.preventDefault();
-  });
-});
+//     if (event.shiftKey) {
+//       value = ShiftKeysMapping[event.key.toUpperCase()]
+//         ? ShiftKeysMapping[event.key.toUpperCase()]
+//         : event.key;
+//     } else {
+//       value = KeysMapping[event.key.toUpperCase()]
+//         ? KeysMapping[event.key.toUpperCase()]
+//         : event.key;
+//     }
+
+//     var newValue = oldValue.slice(0, start) + value + oldValue.slice(end);
+
+//     event.target.value = newValue;
+//     event.target.selectionStart = event.target.selectionEnd = start + 1;
+//     event.preventDefault();
+//   });
+// });
+
+
+alert("Hello world")
