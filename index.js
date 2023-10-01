bc5a88e7cd2692c02fb04bb66d93f5973b1494c2
Daniyal-Qureshi <daniyalqureshi268@gmail.com>
2023-09-30 19:37:02 -0700

    pages styling and improvement

--- index.js
+++ index.js
@@ -7,33 +7,63 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 5000;
 const expire_date = new Date(Date.now() + 1000 * 60 * 60 * 24 * 365 * 10);
-const fs = require('fs').promises;
+const fs = require("fs").promises;
+const expressLayouts = require("express-ejs-layouts");
+const nodemailer = require("nodemailer");
+const { error } = require("console");
 
 require("ejs");
 app.set("view engine", "ejs");
+app.use(expressLayouts);
 app.use(express.static(path.join(__dirname, "public")));
 app.set("views", __dirname + "/views");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
 var openai = "";
 var apiKey = "";
+const RECIPIENT = "daniyalqureshi.cs@gmail.com";
+const SUBJECT = "Urdu GPT Feedback";
 
 const filePath = path.join(__dirname, "public", "keys.json");
 var jsonData = [];
 
 async function readJsonFile() {
   try {
-    const d = await fs.readFile(filePath, 'utf-8');
+    const d = await fs.readFile(filePath, "utf-8");
     jsonData = JSON.parse(d);
   } catch (err) {
     console.error(err);
   }
 }
 
-readJsonFile()
+async function sendEmail(name, message, email, res) {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "daniyalqureshi268@gmail.com",
+      pass: "qtpniwlxhiujxnvv",
+    },
+  });
+  transporter.sendMail(
+    {
+      from: email,
+      to: RECIPIENT,
+      subject: SUBJECT,
+      text: message,
+    },
+    (error, info) => {
+      if (error) {
+        res.redirect("/contact?error=something went wrong");
+      } else {
+        res.redirect("/contact?notice=Your feedback has been received");
+      }
+    }
+  );
+}
+
+readJsonFile();
 
 function checkAPIKey(req, res, next) {
   const responses = req.cookies.responses ? req.cookies.responses : [];
@@ -48,9 +78,7 @@ function checkAPIKey(req, res, next) {
     });
     openai = new OpenAIApi(configuration);
     next();
-  }
-
-  else {
+  } else {
     res.render("home", {
       responses: responses,
       error: "Please provide the API Key",
@@ -74,7 +102,7 @@ app.get("/remove", (req, res) => {
 
 app.get("/set/:key", (req, res) => {
   apiKey = req.params.key;
-  configuration = new Configuration({
+  const configuration = new Configuration({
     apiKey: apiKey,
   });
   openai = new OpenAIApi(configuration);
@@ -124,11 +152,39 @@ app.post("/open", checkAPIKey, async (req, res) => {
     const error = e.response.data.error.message || e.response.data.error.code;
     res.render("home", {
       responses: req.cookies.responses,
-      error:error,
+      error: error,
       data: jsonData,
     });
   }
 });
 
+app.get("/disclaimer", (req, res) => {
+  res.render("disclaimer");
+});
+
+app.get("/privacy", (req, res) => {
+  res.render("privacy");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", { notice: req.query.notice, error: req.query.error });
+});
+
+app.post("/mailer", async (req, res) => {
+  try {
+    const { name, email, message } = req.body;
+    if (name && email && message) {
+      sendEmail(name, message, email, res);
+    } else {
+      res.redirect("/contact?notice=Please provide all the fields");
+    }
+  } catch (e) {
+    res.redirect("/contact?notice=Something went wrong");
+  }
+});
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
